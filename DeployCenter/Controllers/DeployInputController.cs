f1cc7dfe9e1144a4035d639eccec8d949d01813f
Rodney Kam <>
2019-02-16 09:56:25 -0800

    Initial Revision

---
+++ DeployCenter/Controllers/DeployInputController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace DeployCenter.Controllers
+{
+    public class DeployInputController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            ViewData["Message"] = "This page will accept Criteria for Deployment";
+            return View();
+        }
+    }
+}
---
+++ DeployCenter/Models/DeployInputModel.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.ComponentModel;
+using System.Reflection;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace DeployCenter.Models
+{
+    public static class DeployOption
+    {
+        public enum Option
+        {
+            NoOption,
+            [Description("Copy and UnZip Artifacts")]
+            Unzip,
+            [Description("Disable Load Balancer")]
+            DisableLB,
+            [Description("Deploy Core Application and Services")]
+            Core,
+            [Description("Deploy HydroPlatform Services")]
+            Hydro,
+            [Description("Deploy verification Tests")]
+            Verify,
+            [Description("Update Switches")]
+            Switches,
+            [Description("Deploy Direct Messaging Service")]
+            DirectMsg,
+            [Description("Enable Load Balancer")]
+            EnableLB,
+            MAX_VALUE
+        }
+
+        public static string GetDescription(this Option value)
+        {
+            Type type = value.GetType();
+            string name = Enum.GetName(type, value);
+            if (name != null)
+            {
+                FieldInfo field = type.GetField(name);
+                if (field != null)
+                {
+                    DescriptionAttribute attr =
+                           Attribute.GetCustomAttribute(field,
+                             typeof(DescriptionAttribute)) as DescriptionAttribute;
+                    if (attr != null)
+                    {
+                        return attr.Description;
+                    }
+                }
+            }
+            return null;
+        }
+
+    }
+
+    public class DeployInputModel
+    {
+        public string EnvironmentId { get; set; }
+        public string Revision { get; set; }
+        public string[] Servers { get; set; }
+        public bool[] DeployOptions { get; set; }
+
+        public MultiSelectList ServerList { get; set; }
+        public SelectList EnvironmentList { get; set; }
+
+        public DeployInputModel()
+        {
+            DeployOptions = new bool[(int)DeployOption.Option.MAX_VALUE];
+        }
+
+        private void ClearOptions()
+        {
+            for(var i = 0; i < DeployOptions.Length; i++)
+            {
+                DeployOptions[i] = false;
+            }
+        }
+
+    }
+}
