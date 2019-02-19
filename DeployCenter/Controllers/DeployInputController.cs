274b52481ff60ab15d48c1d68275b5615c188d15
Rodney Kam <>
2019-02-18 23:52:04 -0800

    Added DeployTargets Class for Server Info for each domain.

--- DeployCenter/Controllers/DeployInputController.cs
+++ DeployCenter/Controllers/DeployInputController.cs
@@ -3,37 +3,57 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using DeployCenter.Models;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Newtonsoft.Json;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace DeployCenter.Controllers
 {
     public class DeployInputController : Controller
-    { 
-        // GET: /<controller>/
+    {
+        private readonly IHostingEnvironment _hostingEnvironment;
+
+        public DeployInputController(IHostingEnvironment hostingEnvironment)
+        {
+            _hostingEnvironment = hostingEnvironment;
+        }
+
         public IActionResult Index()
         {
+            string webRootPath = _hostingEnvironment.WebRootPath;
+            var deployTargets = new DeployTargets(webRootPath);
+
             var model = new DeployInputModel();
 
+            var envList = deployTargets.GetEnvironmentList();
+            model.EnvironmentList = new List<SelectListItem>();
+            foreach (var env in envList)
+            {
+                var item = new SelectListItem(env, env);
+                model.EnvironmentList.Add(item);
+            }
+
+            var serverList = deployTargets.GetServerList("Stage");
+            model.ServerList = new List<SelectListItem>();
+            foreach (var server in serverList)
+            {
+                var item = new SelectListItem(server.Name, server.Name);
+                model.ServerList.Add(item);
+            }
+
             model.RevisionList = new List<SelectListItem>
-                { new SelectListItem { Selected = false, Text = "19.1.5000.1", Value = "1" },
-                  new SelectListItem { Selected = false, Text = "19.2.6000.1", Value = "2" },
-                  new SelectListItem { Selected = false, Text = "19.3.6500.1", Value = "3" }
-                };
-
-            model.EnvironmentList = new List<SelectListItem>
-                { new SelectListItem { Selected = false, Text = "Stage", Value = "STAGE"},
-                  new SelectListItem { Selected = false, Text = "Integration", Value = "INTEG"},
-                  new SelectListItem { Selected = false, Text = "Production", Value = "PROD"},
-                };
-
-            model.ServerList = new List<SelectListItem>
-                { new SelectListItem { Selected = false, Text = "Server List Goes Here", Value = "1"},
-                };
+            {
+                new SelectListItem("19.1.5000.1", "1"),
+                new SelectListItem("19.2.5100.1", "2"),
+                new SelectListItem("19.3.5200.1", "3"),
+                new SelectListItem("19.4.5300.1", "4"),
+            };
 
             ViewData["Message"] = "This page displays deployment options.";
+        
             return View(model);
         }
     }
--- DeployCenter/Controllers/HomeController.cs
+++ DeployCenter/Controllers/HomeController.cs
@@ -5,11 +5,13 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using DeployCenter.Models;
+using Microsoft.AspNetCore.Hosting;
 
 namespace DeployCenter.Controllers
 {
     public class HomeController : Controller
     {
+
         public IActionResult Index()
         {
             return View();
